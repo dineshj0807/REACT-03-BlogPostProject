61f2f95a1bed876647249693acb98c246fa60ff3
dineshj0807 <dineshj0807@gmail.com>
2026-02-15 12:38:45 +0530

    blog post done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <a href="#privacy">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#t&c">Terms & Conditions</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a>
+        </li>
+        <li>
+          <a href="#more">More Info</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>A Blog Post</h1>
+      <p>
+        Typography is one of the most important elements of front end design and
+        development! In this exercise, we'll play around with some of the common
+        CSS properties to manage and style our fonts by making a nice blog
+        post-like web page.
+      </p>
+      <hr />
+    </header>
+  );
+}
